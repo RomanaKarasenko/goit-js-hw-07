31c0bf01bcae1d8566cbdc72d3b2d97a92509bc3
RomanaKarasenko <143837231+RomanaKarasenko@users.noreply.github.com>
2024-01-11 22:20:15 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,11 @@
-const categories = document.querySelector('#categories');
-const items = categories.querySelectorAll('.item');
+const categories = document.querySelector("#categories");
+const items = categories.querySelectorAll(".item");
 
 console.log(`${items.length}`);
 
-items.forEach(item => {
-    const categoryTitle = item.querySelector('h2').textContent;
-    const subItems = item.querySelectorAll('ul li');
-    console.log(`${categoryTitle}`);
+items.forEach((item) => {
+  const categoryTitle = item.querySelector("h2").textContent;
+  const subItems = item.querySelectorAll("ul li");
+  console.log(`${categoryTitle}`);
   console.log(`${subItems.length}`);
 });
